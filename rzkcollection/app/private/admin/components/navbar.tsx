160aaa1cddb13c29d49ef6b320ea64f64b24070f
Dhefika <dhefikafzr@gmail.com>
2023-05-24 20:42:47 +0700

    Merge branch 'main' of https://github.com/Dhefika/rzkweb

---
+++ rzkcollection/app/private/admin/components/navbar.tsx
@@ -0,0 +1,13 @@
+import '../style.css'
+
+function Navbar() {
+  return (
+    <div className='header'> 
+      <img className='logo-title' src={require ('../assets/Navbarlogo.png')}alt=""/>
+      <img className='profile-title' src={require ('../assets/Profile.png')}alt=""/>
+
+    </div>
+  )
+}
+
+export default Navbar;
