fe169b217fe3687d11e3c096e939f5bc9048290a
NabilAthillah <muhammadnabilathillah@gmail.com>
2023-05-25 02:46:10 +0700

    test client

---
+++ rzkcollection/app/api/api.tsx
@@ -0,0 +1,18 @@
+// api.tsx
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const data = await prisma.variant.findMany();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
